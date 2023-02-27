4a7827790d31ae2e962585bbc4f47670025dcbe2
Tao Yang <yangtaosimon@gmail.com>
2023-02-26 21:25:34 -0800

    Message System version 1

---
+++ WebSocket/chatserver/src/main/java/com/example/chatserver/ChatserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatserverApplication.class, args);
+	}
+
+}
---
+++ WebSocket/chatserver/src/main/java/com/example/chatserver/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.chatserver.model;
+
+public enum Status {
+	JOIN,
+	MESSAGE,
+	LEAVE
+}
+
