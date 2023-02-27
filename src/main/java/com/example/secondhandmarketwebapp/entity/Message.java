bd28447a9e8a43a68a872ff215cddc958e915eae
Serena Liu <yichen.liu0628@gmail.com>
2023-02-27 11:52:04 -0800

    message system not yet finished

---
+++ src/main/java/com/example/secondhandmarketwebapp/entity/Message.java
@@ -0,0 +1,16 @@
+package com.example.secondhandmarketwebapp.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+}
---
+++ src/main/java/com/example/secondhandmarketwebapp/entity/Status.java
@@ -0,0 +1,7 @@
+package com.example.secondhandmarketwebapp.entity;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
